d5d75cddc21dcdd003d7ea884c8f67b2da496968
mishacutota <cutoramihail2@gmail.com>
2026-09-18 19:47:15 +0300

    Add files via upload

---
+++ games.js
@@ -0,0 +1,13 @@
+// ==========================================================
+// 👇 ВАШ СПИСОК ИГР. ДОБАВЛЯЙТЕ ХОТЬ 20 ШТУК!
+// ==========================================================
+const gamesData = [
+    { name: "win67", img: "icons/win67.png", link: "https://mishacutota.github.io/win67/" },
+    { name: "CPP", img: "icons/no.png", link: "https://mishacutota.github.io/ConstructorPreviewPRO/" },
+    { name: "RP-creator", img: "icons/RC.png", link: "https://mishacutota.github.io/RC/" },
+    { name: "2DCLIKER", img: "icons/no.png", link: "https://mishacutota.github.io/2DCLIKER/" },
+    { name: "FHWV-BMC", img: "icons/FH-BMC.png", link: "https://misha-cutora.itch.io/fhwv-bmc" },
+    { name: "MFS-BMC", img: "icons/no.png", link: "https://mishacutota.github.io/MFS-BMC/" },
+    { name: "RPM BMC and Dewiep", img: "icons/RP Merger By Misha_Cutora and Dewiep.png", link: "https://mishacutota.github.io/RP-Merger-By-Misha_Cutora-and-Dewiep/" },
+    { name: "MathX", img: "icons/mathx.png", link: "https://mishacutota.github.io/x-Math-BMC/" }
+];
